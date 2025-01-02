d2e1922de13e74a55b18f66316b179fd64f23342
ranvane <ranvane@gmail.com>
2025-01-03 00:50:01 +0800

    ....

--- commit.sh
+++ commit.sh
@@ -8,14 +8,12 @@ git config --global https.proxy https://127.0.0.1:7890
 git config --global http.proxy 'socks5://127.0.0.1:7890'
 git config --global https.proxy 'socks5://127.0.0.1:7890'
 
-
 echo  "添加文件变动记录"
 git add -A
 git commit -m "...."
 echo -e "\n开始推送远程仓库.................\n"
 git push origin
 echo -e "\n推送远程仓库完成.................\n"
-
 ## 取消代理
 echo  "取消Git代理"
 git config --global --unset http.proxy
